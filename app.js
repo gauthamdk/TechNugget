402aba094c11f1aa9aadb09d581a2c041edb3d71
Gautham <gauthamkumar.gk.gk@gmail.com>
2020-06-20 15:20:49 +0400

    add public and partials directory

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-const express=require("express");
-const app = express();
+const express=require("express"),
+      app = express()
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname+"/public"));
 
 app.get("/", (req, res)=>{
 	res.render("home");
