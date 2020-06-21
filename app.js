38279fd33cbad5cfff4a8c0f9c11342ff39f9c57
Gautham <gauthamkumar.gk.gk@gmail.com>
2020-06-21 22:30:22 +0400

    add bootstrap navbar

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
 
 app.get("/", (req, res)=>{
-	res.render("home");
+	res.redirect("/home");
+})
+
+app.get("/home", (req, res)=>{
+    res.render("home");
 })
 
 app.listen(3000, ()=>{
