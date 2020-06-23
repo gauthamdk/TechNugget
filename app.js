a2d3040416f6d1f162616c48261dc01c0121e293
Gautham <gauthamkumar.gk.gk@gmail.com>
2020-06-23 21:40:20 +0400

    add active link

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const express=require("express"),
-      app = express()
+    app = express(),
+    bodyParser = require("body-parser")
+    
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(function(req, res, next){
+    req.active = req.path.split('/')[1] // [0] will be empty since routes start with '/'
+    next();
+});
 
 app.get("/", (req, res)=>{
 	res.redirect("/home");
 })
 
 app.get("/home", (req, res)=>{
-    res.render("home");
+    res.render("home", {link :req.url.split('/')[1], activeClass: "active"});
 })
 
 app.get("/store", (req, res)=>{
-    res.render("store")
+    res.render("store", {link :req.url.split('/')[1], activeClass: "active"})
 })
 
 app.listen(3000, ()=>{
